2c2a6274942f256637835e56e3e217e825d9ffc5
Meow <108517390+MeowMeow0417@users.noreply.github.com>
2025-08-08 00:49:21 +0800

    visualizing the frontend

---
+++ components/custom/BreakDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BreakDisplay = () => {
+  return (
+    <div>BreakDisplay</div>
+  )
+}
+
+export default BreakDisplay
---
+++ components/custom/ButtonControls.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonControls = () => {
+  return (
+    <div>ButtonControls</div>
+  )
+}
+
+export default ButtonControls
